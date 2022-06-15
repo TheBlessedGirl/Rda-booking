863363fbfd7987edb772e9243df5234f3845266a
Eric SIMBANDUMWE <claireshimwe2012.com>
2022-06-15 13:23:07 +0200

    refactoring

--- src/views/about.js
+++ src/views/about.js
@@ -8,9 +8,14 @@ import "./tour.css";
 const AboutUs =()=>{
     return(
         <>
-<h1>About US</h1>
+        <div style={{backgroundImage:"../assets/images/background.jpg"}}>
+<h1 style={{textAlign:"left",paddingRight:"20px"}}>ABOUT US</h1>
 <div>
-    <img src={image}style={{margin:" 0 40%  "}}/>
+    <img src={image}style={{paddingRight:"20px"}}/>
+</div>
+<p>Gboard is a virtual keyboard app developed by Google for Android and iOS devices. It was first released on iOS in May 2016, followed by a release on Android in December 2016, debuting as a major update to the already-established Google Keyboard app on Android.
+
+Gboard features Google Search, including web results (removed since April 2020)[3] and predictive answers, easy searching and sharing of GIF and emoji content, a predictive typing engine suggesting the next word depending on context, and multilingual language support. Updates to the keyboard have enabled additional functionality, including GIF suggestions, options for a dark color theme or adding a personal image as the keyboard background, support for voice dictation, next-phrase prediction, and hand-drawn emoji recognition. At the time of its launch on iOS, the keyboard only offered support for the English language, with more languages being gradually added in the following months, whereas on Android, the keyboard supported more than 100 languages at the time of release.</p>
 </div>
 </>
 )
